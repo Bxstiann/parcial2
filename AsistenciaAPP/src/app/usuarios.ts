c1d6b1c318d355922fed2fbb84a5dde80223a55e
dedcodex27800 <os.moreno@duocuc.cl>
2024-11-07 12:16:26 -0300

    se elimina la carpeta creada erroneamente del proyecto

--- AsistenciaAPP/src/app/usuarios.ts
+++
@@ -1,7 +0,0 @@
-export class GlobalUsers {
-    public static usuarios = [
-      { username: 'Usuario1', password: 'MiClav3' },
-      { username: 'Usuario2', password: 'OtraClave123' }
-    ];
-  }
-  
